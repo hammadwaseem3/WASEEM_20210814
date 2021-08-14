16b7889e345c29671b99c0e4a134a86d2b67eedf
Hammad Waseem <hammadwaseem@rocketmail.com>
2021-08-14 10:56:45 +0200

    first commit

---
+++ src/main/java/com/company/waseem_20210814/Waseem20210814Application.java
@@ -0,0 +1,13 @@
+package com.company.waseem_20210814;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waseem20210814Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Waseem20210814Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/waseem_20210814/Waseem20210814ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.waseem_20210814;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waseem20210814ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
