6cc0ed5bb0b2a92b9b4bfa421a4b21575308ca4e
Hammad Waseem <hammadwaseem@rocketmail.com>
2021-08-14 11:16:21 +0200

    add video and video category entities

---
+++ src/main/java/com/company/waseem_20210814/entity/Video.java
@@ -0,0 +1,27 @@
+package com.company.waseem_20210814.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Video {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private VideoCategory category;
+
+    private String filePath;
+
+
+}
---
+++ src/main/java/com/company/waseem_20210814/entity/VideoCategory.java
@@ -0,0 +1,17 @@
+package com.company.waseem_20210814.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class  VideoCategory {
+
+    @Id
+    private Integer id;
+
+    private String categoryName;
+
+}
